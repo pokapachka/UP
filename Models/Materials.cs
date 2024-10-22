52f7a0af00600d2da49313bf64fbece5866052ff
semenKulikov <semen.kulikov2000@mail.ru>
2024-10-22 12:30:32 +0500

    Добавьте файлы проекта.

---
+++ Models/Materials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsokinUP.Models
+{
+    public class Materials
+    {
+        public int id { get; set; }
+        public string Material_type { get; set; }
+        public string Name { get; set; }
+        public float Defect_rate { get; set; }
+        public int Supplier { get; set; }
+        public int Quantity_in_packaging { get; set; }
+        public int Unit_of_measurement { get; set; }
+    }
+}
---
+++ Models/Suppliers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsokinUP.Models
+{
+    public class Suppliers
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public int INN { get; set; }
+    }
+}
