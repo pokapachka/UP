e386e651d6bd2c9ae446d742738801cd384bff20
semenKulikov <semen.kulikov2000@mail.ru>
2024-10-23 12:54:29 +0500

    Создана иерархия папок. Осокин

---
+++ Models/Partners_products.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsokinUP.Models
+{
+    class Partners_products
+    {
+    }
+}
---
+++ Models/Product_type.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsokinUP.Models
+{
+    class Product_type
+    {
+    }
+}
